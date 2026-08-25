66b5ec15e72f2fdc3acc1c91ec8f24d22964d5e8
aya skaro <ayaesk243@gmail.com>
2026-08-25 13:57:54 -0700

    Add files via upload

---
+++ tiktok_game.py
@@ -0,0 +1,16 @@
+def convert(row,column):
+                           
+
+
+
+while True:
+    print(table)
+    first_player=int(input(" Player X, enter your move (row and column): "))
+    if first_player ==
+
+
+
+
+
+
+
